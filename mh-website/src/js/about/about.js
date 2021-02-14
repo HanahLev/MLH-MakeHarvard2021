da55c3d23d7383c1fa4e187708b15265db438b09
brandon <brandonchoi2016@gmail.com>
2021-02-14 03:33:26 -0800

    fixed about us page

--- mh-website/src/js/about/about.js
+++ mh-website/src/js/about/about.js
@@ -1,11 +1,14 @@
-
+import { aboutussvg } from '../../assets/asset'
+import '../../sass/solutions.scss'
 
 export default function About() {
 
 
     return (
         <div className='about-wrapper'>
-            about wrapper
+            <div className='aboutus-wrapper'>
+                {aboutussvg}
+            </div>
         </div>
     )
 
--- mh-website/src/js/solutions/solutions.js
+++ mh-website/src/js/solutions/solutions.js
@@ -1,4 +1,4 @@
-import { aboutussvg } from '../../assets/asset'
+
 import '../../sass/solutions.scss'
 
 
@@ -7,9 +7,7 @@ export default function Solutions() {
 
     return (
         <div className='solutions-wrapper'>
-            <div className='aboutus-wrapper'>
-                {aboutussvg}
-            </div>
+
         </div>
     )
 
