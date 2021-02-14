1453aa5c6468517557bf5c5de40a549aa20bbd5c
brandon <brandonchoi2016@gmail.com>
2021-02-14 01:12:34 -0800

    added scrollbar to trackmap lis

--- mh-website/src/js/dashboard/trackmap.js
+++ mh-website/src/js/dashboard/trackmap.js
@@ -1,21 +1,43 @@
 import { useHistory, useParams } from 'react-router-dom';
 import { GoogleMap, Marker } from "react-google-maps"
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import PerfectScrollbar from 'react-perfect-scrollbar'
 
 import '../../sass/trackmap.scss'
+import { useState, useEffect } from 'react';
+
+import { leftArrow, location } from '../../assets/asset'
+import useAxios from 'axios-hooks'
+
 
 export default function TrackMap() {
     let { zipcode } = useParams();
-    if (zipcode) {
-        //send request to api
+    var zip = zipcode
+    if (!zipcode) {
+        zip = 90502
     }
+    //guarentees zip code
+    const [tracks, setTracks] = useState()
+
+    const [{ data, loading, error }, refetch] = useAxios(
+        'https://api.get-tested-covid19.org/api/v1/public/public-test-centers/zip/' + zip
+    )
+    //if zipcode is avail and tracks is not
+    // then it is loading
+
 
     return (
         <div className='trackmap-wrappers'>
 
             <div className='trackmap-left '>
                 <div className='trackmap-back-btn'>
-                    back
+                    {leftArrow} back
                 </div>
+                <PerfectScrollbar>
+                    <div className='trackmap-list'>
+
+                    </div>
+                </PerfectScrollbar>
             </div>
             <div className='trackmap-right'>
 
@@ -24,4 +46,21 @@ export default function TrackMap() {
 
     )
 
+}
+
+
+function TrackMapItem(props) {
+    //get list of items
+    return (
+        <div className='trackmap-item-wrapper'>
+            <span className='index'>
+                {props.index}.
+            </span>
+            <span>
+                {location}{props.name}
+            </span>
+
+        </div>
+
+    )
 }
